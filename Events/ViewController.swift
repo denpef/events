3d348f4165946ee18ab2ac0268d87444bde35e32
Denis Efimov <denis.efimov@itechart-group.com>
2019-11-27 16:12:58 +0300

    Initial Commit

---
+++ Events/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Events
+//
+//  Created by Denis Efimov on 11/27/19.
+//  Copyright © 2019 Denis Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
